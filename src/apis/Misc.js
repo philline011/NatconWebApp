d1b138051cae129fe68258e3b5172cd6864ca4a4
John David Guevarra <david063095@gmail.com>
2023-03-17 13:05:08 +0800

    + Fixed resources

--- src/apis/Misc.js
+++ src/apis/Misc.js
@@ -1,32 +1,32 @@
-import {API_URL} from '../config'
+import { API_URL } from '../config'
 import axios from 'axios'
 
 export const saveFeedback = (input, callback) => {
-    const api_link = `${API_URL}/api/feedback/save_feedback`;
-    axios
-      .post(api_link, input)
-      .then(response => {
-        const {data} = response;
-        console.log('Save Feedback', data);
-        callback(data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
+  const api_link = `${API_URL}/api/feedback/save_feedback`;
+  axios
+    .post(api_link, input)
+    .then(response => {
+      const { data } = response;
+      console.log('Save Feedback', data);
+      callback(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
 
 export const getFilesFromFolder = (folder, callback) => {
-    const api_link = `${API_URL}/api/misc/get_files/${folder}`;
-    axios
-      .get(api_link)
-      .then(response => {
-        if (response.data.status === true) {
-            callback(response.data.data);
-        }
-      })
-      .catch(error => {
-        console.error(error);
-      });
+  const api_link = `${API_URL}/api/misc/get_files/${folder}`;
+  axios
+    .get(api_link)
+    .then(response => {
+      if (response.data.status === true) {
+        callback(response.data.data);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
 
 export const uploadHazardMaps = (input, callback) => {
@@ -34,8 +34,22 @@ export const uploadHazardMaps = (input, callback) => {
   axios
     .post(api_link, input)
     .then(response => {
-      const {data} = response;
+      const { data } = response;
       console.log('Uploaded map', data);
+      callback(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+export const uploadResources = (input, callback) => {
+  const api_link = `${API_URL}/api/upload/save_resources`;
+  axios
+    .post(api_link, input)
+    .then(response => {
+      const { data } = response;
+      console.log('File uploaded', data);
       callback(data);
     })
     .catch(error => {
@@ -49,7 +63,7 @@ export const getNumberOfFiles = (folder, callback) => {
     .get(api_link)
     .then(response => {
       if (response.data.status === true) {
-          callback(response.data.data);
+        callback(response.data.data);
       }
     })
     .catch(error => {
